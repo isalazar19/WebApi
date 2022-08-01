74f43c1e51ce27d038a96bd6f5fdc0b8ff09ca1b
isalazar19 <ivan.condore@gmail.com>
2022-07-31 21:28:38 -0400

    Se agrega el proyecto de VisualStudio

---
+++ Controllers/AutosController.cs
@@ -0,0 +1,35 @@
+﻿using ConectarDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class AutosController : ApiController
+    {
+        private InverntarioEntities dbContext = new InverntarioEntities();
+
+        //Visualiza todos los registros (https://localhost:44374/Api/Autos)
+        [HttpGet]
+        public IEnumerable<Autos> Get()
+        {
+            using (InverntarioEntities inventarioentities = new InverntarioEntities())
+            {
+                return inventarioentities.Autos.ToList();
+            }
+        }
+
+        //Visualiza solo un registro (https://localhost:44374/Api/Autos/1)
+        [HttpGet]
+        public Autos Get(int id)
+        {
+            using (InverntarioEntities inverntarioEntities = new InverntarioEntities())
+            {
+                return inverntarioEntities.Autos.FirstOrDefault(e => e.Id == id);
+            }
+        }
+    }
+}
